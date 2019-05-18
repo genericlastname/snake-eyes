ed55d1fc42e13d5b9a611c6032dcb26201e4584a
Kyle Brown <brownkylej@gmail.com>
2019-05-17 19:13:44 -0500

    Add roll command

--- bot.py
+++ bot.py
@@ -1,36 +1,42 @@
 import discord
 from discord.ext import commands
-import datetime
+import random
 
 client = discord.Client()
-TOKEN = 'NTc3OTU2ODM5NzYxNjQxNDky.XNsy-g.dvstEBH_6tIC0i-1NkIaBZU0Gk0'
+TOKEN = 'NTc5MDg2Njg4Nzc3Nzk3NjQy.XN9EBw.AY3vl4svrZc_LPkglMkr3ohiJz8'
 bot = commands.Bot(command_prefix='!')
 
-# deadlines = []
+@bot.command()
+async def roll(ctx, *args):
+    # if argument is a valid die
+    if args[0][0] == 'd':
+        faces = int(args[0][1:])
+        value = 0
+        
+        random.seed()
+        if faces != 00:
+            # normal dice
+            value = random.randint(1, faces)
+        else:
+            # percentile die
+            value = random.randint(1, 10) * 10
 
-# @bot.command()
-# async def upcoming(ctx):
-#     for d in deadlines:
-#         await ctx.send(d.info())
+        output = ''
+        if faces == 20:
+            # add pithy comments for critical success and failure
+            if value == 20:
+                output = 'Critical success! A natural 20!'
+                print(output)
+            elif value == 1:
+                output = 'Critical failure! You rolled a 1!'
+            else:
+                output = f'You rolled a...\n**{value}**'
 
-# @bot.command()
-# async def deadline(ctx, name, date_str, time_str, ampm):
-#     combined = f'{date_str} {time_str} {ampm.upper()}'
-#     print(combined)
-#     dt = datetime.datetime.strptime(combined, '%Y-%m-%d %I:%M %p')
-#     deadlines.append(Deadline(name, dt))
-#     await ctx.send(f'Adding {name} to calender')
+        else:
+            output = f'You rolled a...\n**{value}**'
 
-# @bot.command()
-# async def delete_deadline(ctx, name):
-#     results = []
-#     for d in deadlines:
-#         if d.name != name:
-#             results.append(d)
-
-#     deadlines = results
-#     await ctx.send(f'Deleted deadline {name}')
+        print(output)
+        await ctx.send(output)
 
 
 bot.run(TOKEN)
-
